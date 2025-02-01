7a48f6dc99d374a1d86bd073009c3e99e06f4eec
OsmelJulio <osmeljulio247@gmail.com>
2025-02-01 13:11:45 -0500

    cambio 2

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded',function(){
    const button = document.getElementById ('changeTextButton');
    button.addEventListener('click', function(){
        const introSection=document.getElementById('intro');
-       introSection.innerHTML ='el texto a cambiado despues de hacer clik ';
+       introSection.innerHTML =' <p>el texto a cambiado despues de hacer clik</p> ';
    });
 });
