3feeb0d6c4d53c0430edc3002fb12c4fe2e5f582
OsmelJulio <osmeljulio247@gmail.com>
2025-02-01 11:35:09 -0500

    Button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded",function(){
-   const button = document.getElementById ("changeTextbutton")
+   const button = document.getElementById ("changeTextButton")
    button.addEventListener("click", function(){
        const introSection=document.getElementById("intro")
  introSection.innerHTML="<p>el texto a cambiado despues de hacer clik </p>"
